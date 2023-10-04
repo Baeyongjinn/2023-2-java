47be2cf632a162365a6e0dfccd579d45040c84e0
배용진 <dydwls2270@naver.com>
2023-10-04 18:26:35 +0900

    class2

--- src/main/java/com/green/day6/ch4/FlowEx28.java
+++ src/main/java/com/green/day6/ch4/FlowEx28.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class FlowEx28 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        //
         int input = 0;
         int answer = (int) (Math.random() * 100) + 1;
-            System.out.println("answer : " + answer);
+        System.out.println(answer);
+        //
+        Scanner scan = new Scanner(System.in);
+        //
         while (true) {
             System.out.print("1~100사이의 정수를 입력하세요 >> ");
             input = scan.nextInt();
-            if(input == answer ) {
-            } else if (input > answer) {
+            if(input > answer ) {
                 System.out.println("Down");
-            } else if (input < answer) {
+            } else if(input < answer) {
                 System.out.println("up");
-
+            }else {
+                System.out.println("끝!!");
+                break;
             }
-            System.out.println("끝!!");
         }
 
 
@@ -28,8 +31,8 @@ public class FlowEx28 {
 
         /*
         1~100 사시의 정수를 입력하세요 >>
-        입력한 숫자보다 정답이 크다 > "Up"
-        입력한 숫자보다 정답이 작다 > "Down"
+        입력한 숫자 보다 정답이 크다 > "Up"
+        입력한 숫자 보다 정답이 작다 > "Down"
         맞출떄 까지 반복
          */
     }
